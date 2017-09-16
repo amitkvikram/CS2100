8de7589d9732d19ea95d0b9c2c7e45324fecbdf6
amit <amittengradepoint@gmail.com>
2017-09-16 14:51:01 +0530

    ex3

---
+++ week3b/ex3.c
@@ -0,0 +1,64 @@
+/*
+  Problem Name:Ex3
+  Programmers'Name: Amit Vikram Singh
+  Roll No.:111601001
+  Date:12/09/2017
+*/
+
+#include<stdio.h>
+int inc=0;
+
+void swap(int *a, int *b){
+      int temp = *a;
+      *a = *b;
+      *b = temp;
+}
+
+int partition(int *arr, int p, int r){
+      inc++;
+      int i = p;
+      for(int j=p; j<r; j++){
+            if(arr[j]<=arr[r]){
+                  swap(&arr[i], &arr[j]);
+                  i++;
+            }
+      }
+
+      swap(&arr[r], &arr[i]);   //calling swap function to adjust the position of pivot
+      return i;
+}
+
+
+void quick_sort(int *arr, int p, int r){
+      if(p<r){
+            int q = partition(arr, p, r); //calling function partition to spli the array in 2 parts.elements in left part>arr[r]
+                                          //whereas elments in right<arr[r]
+            quick_sort(arr, p, q-1);      //calling quick_sort function to sort the subarray.
+            quick_sort(arr, q+1, r);
+      }
+}
+
+//main function begins here
+int main(int argc, char const *argv[]) {
+
+      printf("Enter length of array: ");
+ 	int n; 				//length of array
+ 	scanf("%d", &n);
+ 	int arr[n];			//array to store the elements
+ 	printf("Enter elements of array: ");
+ 	int i=0;
+ 	for(i=0; i<n; i++){
+ 		scanf("%d", &arr[i]);
+ 	}
+
+      quick_sort(arr, 0, n-1);  //callinf function quick_sort to sort the array
+
+      printf("Sorted Array: ");
+ 	for(i=0; i<n; i++){
+ 		printf("%d ", arr[i]);
+ 	}
+ 	printf("\n%d",inc);
+      return 0;
+}
+
+//main function ends here
